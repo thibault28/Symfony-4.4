03d1c577c5a9fb72a2d119d03726e6db05b8975f
Admin <admin@admin.fr>
2020-05-20 13:41:31 +0200

    Add Fixtures and Faker

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Symfony-4.4
+
+Cloner le répository
+Faire un composer install
+Faire un yarn install
+Faire un yarn encore dev
+Dupliquer le fichier .env et le nommer .env.local et changer les informations de la base de données
+
+# Bundle
+
+Webpack encore : Bootstrap JQuery
+Fixture : Ajout de contenu dans la BDD
+Faker : Génération de fausses données
---
+++ src/DataFixtures/ExampleFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Example;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ExampleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Factory::create('fr_FR');
+
+        for ($i = 0; $i < 50; $i++) {
+            $example = new Example();
+            $example->setName($faker->name);
+            $manager->persist($example);
+
+            $manager->flush();
+        }
+    }
+}
