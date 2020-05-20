9eb37ec74f6fdf35402d55b82351b15fba81e2f6
Admin <admin@admin.fr>
2020-05-20 12:32:52 +0200

    Add Webpack Encore

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import "../css/bootstrap.scss";
+import "../css/app.scss";
+
+const $ = require("jquery");
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require("bootstrap");
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+//import $ from "jquery";
+
+$(document).ready(function () {
+  //Import the js here
+});
